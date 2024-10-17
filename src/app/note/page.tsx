7773bfb4a80abc4f0e90d4b51948c67d2085fa64
AlejandroDalzotto <aledalzotto15@gmail.com>
2024-10-17 13:29:39 -0300

    add information about the file

--- src/app/note/page.tsx
+++ src/app/note/page.tsx
@@ -1,7 +1,9 @@
 "use client";
 import MarkdownContent from "@/components/MarkdownContent";
-import { remove } from "@/lib/markdown.service";
+import { getMarkdownInformation, remove } from "@/lib/markdown.service";
+import { MarkdownFileInformation } from "@/lib/types";
 import { useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
 
 // Apparently not supported.
 // https://nextjs.org/docs/app/building-your-application/deploying/static-exports#unsupported-features
@@ -17,9 +19,19 @@ import { useSearchParams } from "next/navigation";
 export default function SingleNotePage() {
 
   const searchParams = useSearchParams()
-  const slug = searchParams.get("slug")
+  const slug = searchParams.get("slug")!
+  const [markdownInformation, setMarkdownInformation] = useState<MarkdownFileInformation | null>(null)
 
-  const deleteNote = async (slug: string) => {
+  useEffect(() => {
+
+    const load = async () => {
+      const information = await getMarkdownInformation(slug)
+      setMarkdownInformation(information)
+    }
+    load();
+  }, [slug])
+
+  const deleteNote = async () => {
 
     await remove(slug)
 
@@ -29,18 +41,28 @@ export default function SingleNotePage() {
 
   return (
     <div className="w-full h-full flex flex-col p-1 gap-y-2">
+      <header className="w-full">
+        <h1 className="font-geist-mono text-lg">File title:{" "}
+          {markdownInformation ? (
+            <span className="text-rose-600">{markdownInformation.title}</span>
+          ) : (
+            <span className="text-rose-600">undefined (Not found)</span>
+          )}
+        </h1>
+      </header>
       <section className="w-full h-full overflow-y-auto p-2 lg:px-6 lg:py-4 border rounded-lg border-white/5">
-        <MarkdownContent slug={slug!} />
+        <MarkdownContent slug={slug} />
       </section>
       <footer className="w-full flex items-center justify-between">
         <button
-          onClick={() => deleteNote(slug!)}
-          className="w-8 h-8 p-1 bg-red-400 transition-all border rounded-md opacity-0 hover:scale-110 bottom-2 right-2 border-white/5 bg-white/5 group-hover/card:opacity-100"
+          onClick={deleteNote}
+          className="w-8 h-8 p-1 transition-all border rounded-md hover:scale-110 border-white/5 bg-white/5"
         >
           <svg className="w-full h-full fill-neutral-50">
             <use xlinkHref="/sprites.svg#delete" />
           </svg>
         </button>
+        {markdownInformation && <span className="font-geist-mono text-sm text-neutral-400">last update: {markdownInformation.updatedAt}</span>}
       </footer>
     </div>
   )
