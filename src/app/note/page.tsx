26657f9ab39e7221889a52877ec30759206308b2
AlejandroDalzotto <aledalzotto15@gmail.com>
2025-06-04 16:36:19 -0300

    style: refine layout and improve button hover effects in note page

--- src/app/note/page.tsx
+++ src/app/note/page.tsx
@@ -63,39 +63,38 @@ const Wrapper = () => {
     );
 
   return (
-    <div className="w-full h-full flex flex-col p-1 gap-y-2">
+    <div className="flex flex-col w-full h-full p-1 gap-y-2">
       <header className="w-full">
-        <h1 className="font-geist-mono font-bold text-lg text-rose-400">
+        <h1 className="text-lg font-bold font-geist-mono text-rose-400">
           {metadata ? metadata.title : "undefined (Not found)"}
         </h1>
       </header>
-      <section className="w-full h-full overflow-y-auto p-2 lg:px-6 lg:py-4 border rounded-lg border-white/5">
+      <section className="w-full h-full p-2 overflow-y-auto border rounded-lg lg:px-6 lg:py-4 border-white/5">
         {content && metadata && metadata.fileExtension === FileExtension.MARKDOWN ? (
           <MarkdownContent content={content} />
         ) : (
-          <div className="whitespace-pre-line prose prose-invert prose-img:rounded-lg prose-video:rounded-lg">
+          <div className="prose whitespace-pre-line prose-invert prose-img:rounded-lg prose-video:rounded-lg">
             {content}
           </div>
         )}
       </section>
-      <footer className="w-full flex items-center justify-between">
-        <div className="flex gap-x-4 items-center">
+      <footer className="flex items-center justify-between w-full">
+        <div className="flex items-center gap-x-4">
           <button
             onClick={deleteNote}
-            className="w-8 h-8 transition-all border rounded-md group/delete hover:scale-110 border-white/5 bg-white/5"
+            className="w-8 h-8 transition-all border rounded-md border-white/5 hover:bg-red-500/5 hover:border-red-500/5 group/delete hover:scale-110 bg-white/5"
           >
-            <IconDelete size={32} className="fill-neutral-50 p-0.5 group-hover/delete:fill-red-400 transition-colors" />
-
+            <IconDelete size={32} className="fill-neutral-50 p-0.5 group-hover/delete:fill-red-600 transition-colors" />
           </button>
           <Link
             title={`Edit ${metadata?.title ?? "undefined"}`}
             href={`/editor/edit?tag=${tag}&ext=${extension}`}
-            className="w-8 h-8 transition-all border rounded-md group/edit hover:scale-110 border-white/5 bg-white/5"
+            className="w-8 h-8 transition-all border rounded-md group/edit hover:bg-green-500/5 hover:border-green-500/5 hover:scale-110 border-white/5 bg-white/5"
           >
-            <IconEdit size={32} className="fill-neutral-50 p-0.5 group-hover/edit:fill-emerald-400 transition-colors" />
+            <IconEdit size={32} className="fill-neutral-50 p-0.5 group-hover/edit:fill-green-600 transition-colors" />
           </Link>
         </div>
-        {metadata && <span className="font-geist-mono text-sm text-neutral-400">last update: {getLocalDateString(metadata.updatedAt)}</span>}
+        {metadata && <span className="text-sm font-geist-mono text-neutral-400">last update: {getLocalDateString(metadata.updatedAt)}</span>}
       </footer>
     </div>
   )
