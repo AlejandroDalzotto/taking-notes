0b90ed8c977529dffa7f3540ef8264fcd1f96d7a
AlejandroDalzotto <aledalzotto15@gmail.com>
2025-06-10 01:30:33 -0300

    refactor: replace form handling with EditorForm component in CreateMdNotePage and CreateTxtNotePage

--- src/app/editor/create/md/page.tsx
+++ src/app/editor/create/md/page.tsx
@@ -2,58 +2,21 @@
 
 import ButtonPreviewHtml from "@/components/ButtonPreviewHtml";
 import ButtonPreviewMarkdown from "@/components/ButtonPreviewMarkdown";
+import EditorContainer from "@/components/editor/EditorContainer";
+import EditorForm from "@/components/editor/EditorForm";
 import MDEditor from "@/components/MDEditor";
-import { FileExtension, NoteEntry } from "@/lib/definitions";
-import { createNote } from "@/lib/notes.service";
-import { useRouter } from "next/navigation";
-import { type FormEvent } from "react";
-import { toast } from "sonner";
+import { NoteExtension } from "@/lib/definitions";
 
 export default function CreateMdNotePage() {
 
-  const router = useRouter()
-
-  const handleAction = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-    const formData = new FormData(e.currentTarget);
-
-    const newEntry: NoteEntry = {
-      title: formData.get("title") as string,
-      content: formData.get("content") as string,
-      fileExtension: FileExtension.MARKDOWN,
-    };
-    const [error, message] = await createNote(newEntry)
-
-    if (!error) {
-      toast.success(message)
-      router.push("/notes")
-
-      return
-    }
-
-    toast.error(error.message)
-  }
-
   return (
-    <form onSubmit={(e) => handleAction(e)} className="flex flex-col w-full grow">
-      <input
-        spellCheck={false}
-        autoComplete="off"
-        name="title"
-        placeholder="Title..."
-        className="w-full p-4 text-xl font-mono transition-colors bg-transparent outline-none placeholder:text-neutral-400 hover:bg-white/[0.01] focus:bg-white/5"
-      />
-      <MDEditor />
-
-      <div className="flex items-center justify-between w-full">
-        <div className="flex items-center gap-x-4">
+    <EditorContainer>
+      <EditorForm extension={NoteExtension.MARKDOWN} buttons={
+        <>
           <ButtonPreviewHtml />
           <ButtonPreviewMarkdown />
-        </div>
-        <button type="submit" className="px-3 py-2 mt-5 transition-colors border rounded-md border-white/5 hover:bg-white/5 w-fit">
-          create
-        </button>
-      </div>
-    </form>
+        </>
+      } contentField={<MDEditor />} />
+    </EditorContainer>
   )
 }
--- src/app/editor/create/txt/page.tsx
+++ src/app/editor/create/txt/page.tsx
@@ -1,99 +1,20 @@
 "use client";
 
 import ButtonPreviewHtml from "@/components/ButtonPreviewHtml";
-import { useDraft } from "@/context/draft-provider";
-import { useEditor } from "@/context/editor-provider";
-import { FileExtension, NoteEntry } from "@/lib/definitions";
-import { createNote } from "@/lib/notes.service";
-import { useRouter } from "next/navigation";
-import { useEffect, useRef, type FormEvent } from "react";
-import { toast } from "sonner";
+import EditorContainer from "@/components/editor/EditorContainer";
+import EditorForm from "@/components/editor/EditorForm";
+import PlainContentField from "@/components/editor/PlainContentField";
+import { NoteExtension } from "@/lib/definitions";
 
 export default function CreateTxtNotePage() {
 
-  const router = useRouter()
-  const { noteEditorData, resetNoteEditor, setInitialNoteEditor, setNoteEditor } = useEditor()
-  const { setDraft, draft } = useDraft()
-  const noteRef = useRef(noteEditorData)
-
-
-  const handleAction = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-    const formData = new FormData(e.currentTarget);
-
-    const newEntry: NoteEntry = {
-      title: formData.get("title") as string,
-      content: formData.get("content") as string,
-      fileExtension: FileExtension.PLAINTEXT,
-    };
-    const [error, message] = await createNote(newEntry)
-
-    if (!error) {
-      toast.success(message)
-      resetNoteEditor()
-      router.push("/notes")
-
-      return
-    }
-
-    toast.error(error.message)
-  }
-
-  useEffect(() => {
-    if (draft?.data.title || draft?.data.content) {
-      console.log({ draft })
-      setInitialNoteEditor(draft.data)
-    }
-  }, [])
-
-  useEffect(() => {
-    noteRef.current = noteEditorData;
-  }, [noteEditorData]);
-
-  useEffect(() => {
-    return () => {
-      if (noteRef.current) {
-        setDraft({
-          data: noteRef.current,
-          extension: FileExtension.PLAINTEXT,
-          tag: null,
-        });
-      }
-    };
-  }, []);
-
   return (
-    <form onSubmit={handleAction} className="flex flex-col w-full grow">
-      <input
-        value={noteEditorData?.title}
-        onChange={(e) => setNoteEditor({
-          title: e.target.value,
-          content: noteEditorData?.content ?? ""
-        })}
-        spellCheck={false}
-        autoComplete="off"
-        name="title"
-        placeholder="Title..."
-        className="w-full p-4 text-xl font-mono transition-colors bg-transparent outline-none placeholder:text-neutral-400 hover:bg-white/[0.01] focus:bg-white/5"
-      />
-      <textarea
-        value={noteEditorData?.content}
-        onChange={(e) => setNoteEditor({
-          title: noteEditorData?.title ?? "",
-          content: e.target.value
-        })}
-        autoComplete="off"
-        name="content"
-        spellCheck={false}
-        placeholder="Write about something..."
-        className="p-4 overflow-y-auto font-mono text-lg transition-colors bg-transparent outline-none resize-none grow text-start placeholder:text-neutral-400 hover:bg-white/5"
-      />
-      <div className="flex items-center justify-between w-full">
-        <ButtonPreviewHtml />
-        <button type="submit" className="px-3 py-2 mt-5 transition-colors border rounded-md border-white/5 hover:bg-white/5 w-fit">
-          create
-        </button>
-      </div>
-    </form>
+    <EditorContainer>
+      <EditorForm extension={NoteExtension.PLAINTEXT} buttons={
+        <>
+          <ButtonPreviewHtml />
+        </>
+      } contentField={<PlainContentField />} />
+    </EditorContainer>
   )
 }
