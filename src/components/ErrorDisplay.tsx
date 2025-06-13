ca19ff922f232159ddf368a683a04b9853fa0897
AlejandroDalzotto <aledalzotto15@gmail.com>
2025-06-13 18:20:56 -0300

    refactor: remove ErrorDisplay and MDEditor components to simplify codebase

--- src/components/ErrorDisplay.tsx
+++
@@ -1,7 +0,0 @@
-export default function ErrorDisplay({ message }: { message: string }) {
-  return (
-    <div className="flex justify-center items-center w-full h-full">
-      <p className="text-neutral-300 text-lg font-semibold">{message}</p>
-    </div>
-  )
-}
