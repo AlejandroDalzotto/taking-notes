0e5ce3949cb335dbb30c96d0358d02257ee27030
AlejandroDalzotto <aledalzotto15@gmail.com>
2025-06-19 18:54:54 -0300

    refactor: remove unused NoteExtension imports and extensionsOptions for cleaner constants file

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,4 @@
-import { NavLink, NoteExtension } from "@/lib/definitions";
+import { NavLink } from "@/lib/definitions";
 
 export const navLinks: NavLink[] = [
   {
@@ -20,19 +20,6 @@ export const navLinks: NavLink[] = [
     icon: "add"
   }
 ];
-
-export const extensionsOptions = [
-  {
-    value: NoteExtension.MARKDOWN,
-    label: "markdown",
-    colorClass: "text-indigo-500",
-  },
-  {
-    value: NoteExtension.PLAINTEXT,
-    label: "plain text",
-    colorClass: "text-orange-500",
-  }
-]
 
 export const MIN_LENGTH_TITLE = 3;
 export const MAX_LENGTH_TITLE = 45;
