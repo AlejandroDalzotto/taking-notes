c7254c4ed9dc227cd912c6968d3adfcbb1550563
AlejandroDalzotto <aledalzotto15@gmail.com>
2026-01-30 11:16:19 -0300

    simplify home page to be the new editor page.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,53 +1,61 @@
-import Grainy from "@/components/Grainy";
-import Navbar from "@/components/Navbar";
-import DraftProvider from "@/context/draft-provider";
-import MigrationProvider from "@/context/migration-provider";
-import { ModalProvider } from "@/context/modal-provider";
-import UpdaterProvider from "@/context/update-provider";
-import type { Metadata } from "next";
+"use client";
+
 import { Exo_2 } from "next/font/google";
 import { Toaster } from "sonner";
 import "./globals.css";
+import Footer from "@/components/footer";
+import AppHeader from "@/components/app-header";
+import MenuBar from "@/components/menu-bar";
+import { useEditorActions } from "@/stores/editor";
+import { useEffect } from "react";
+// import UpdaterRunner from "@/components/updater-runner";
+import MigrationRunner from "@/components/migration-runner";
+import { useMigrationStatus } from "@/stores/migration";
 
 const exo2 = Exo_2({
-  variable: '--font-exo-2',
-  style: 'normal',
-  weight: 'variable',
-  subsets: ['latin']
-})
-
-export const metadata: Metadata = {
-  title: "Taking Notes",
-  description: "Your best way to keep in mind your thoughts through a easy-to-use UI.",
-};
+  variable: "--font-exo-2",
+  style: "normal",
+  weight: "variable",
+  subsets: ["latin"],
+});
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const { initialize } = useEditorActions();
+  const status = useMigrationStatus();
+
+  useEffect(() => {
+    console.log({ status });
+    if (status === "complete") {
+      initialize();
+    }
+  }, [status]);
+
   return (
     <html lang="en">
       <body
-        className={`${exo2.variable} antialiased flex flex-col font-exo-2 w-screen relative p-4 h-screen text-neutral-50 bg-neutral-950`}
+        className={`${exo2.variable} antialiased grid grid-rows-[min-content_1fr_min-content] font-exo-2 w-screen relative h-screen text-neutral-50 bg-neutral-950`}
       >
-        <Grainy />
-        <UpdaterProvider>
-          <MigrationProvider>
-            <DraftProvider>
-              <ModalProvider>
-                <Navbar />
-                <main className="flex w-full h-full overflow-x-hidden">
-                  {children}
-                </main>
-                <Toaster position="bottom-left" toastOptions={{
-                  unstyled: true,
-                  className: "flex border border-white/10 min-h-16 text-sm items-center gap-x-2 px-3 py-2 rounded-md bg-neutral-900 text-neutral-50 fill-neutral-50"
-                }} />
-              </ModalProvider>
-            </DraftProvider>
-          </MigrationProvider>
-        </UpdaterProvider>
+        <header className="row-span-1">
+          <AppHeader />
+          <MenuBar />
+        </header>
+        <main className="flex flex-col relative overflow-hidden row-span-2">{children}</main>
+        <Footer />
+        <Toaster
+          position="bottom-left"
+          toastOptions={{
+            unstyled: true,
+            className:
+              "flex border border-white/10 min-h-16 text-sm items-center gap-x-2 px-3 py-2 rounded-md bg-neutral-900 text-neutral-50 fill-neutral-50",
+          }}
+          visibleToasts={1}
+        />
+        {/*<UpdaterRunner />*/}
+        <MigrationRunner />
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,15 @@
-import { IconHistory } from "@/components/Icons";
-import Link from "next/link";
+"use client";
+
+import Editor from "@/components/editor";
+import Welcome from "@/components/welcome";
+import { useCurrent } from "@/stores/editor";
 
 export default function Home() {
-  return (
-    <div className="flex flex-col grow pt-20 justify-center min-h-full max-h-full bg-white/[.02] rounded-xl px-14 relative">
-      <IconHistory size={384} className="absolute -translate-x-1/2 -translate-y-1/2 fill-neutral-50/[0.03] -z-10 top-1/2 left-1/2" />
-      <header>
-        <h1 className="mb-5 text-3xl">Welcome home!</h1>
-      </header>
-      <section className="mb-5">
-        <p><strong>Taking notes</strong> is an app that helps you to write your ideas with in an intuitive way.</p>
-      </section>
-      <Link
-        target="_blank"
-        className="text-sm font-light transition-colors hover:text-blue-500"
-        href="https://github.com/AlejandroDalzotto"
-      >
-        Made by Alejandro
-      </Link>
-    </div>
-  );
+  const current = useCurrent();
+
+  if (!current) {
+    return <Welcome />;
+  }
+
+  return <Editor />;
 }
